99bbe67327f03df0675f36d768d3fb101a6dd5d9
Saurabh K Kedar <68300121+saurabh6893@users.noreply.github.com>
2026-01-06 02:47:35 +0530

    fix: add proper error handling in user schema pre-save hook

--- server/src/model/user.model.js
+++ server/src/model/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+
 const userSchema = new Schema(
   {
     username: {
@@ -32,9 +33,14 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
 
-  next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error); // Pass error to next middleware
+  }
 });
 
 userSchema.methods.comparePassword = async function (password) {
